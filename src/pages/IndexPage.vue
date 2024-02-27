7c1a8710eff13b61774655664d9bdcb1e73e86b2
unknown <greimerabelguzman29@gmail.com>
2024-02-26 23:17:21 -0400

    Inicialiando el proyecto bien chulito

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <q-page>
+    <q-tabs v-model="tab" dense class="text-grey" active-color="primary" indicator-color="primary" align="justify"
+      narrow-indicator>
+      <q-tab name="staff" label="Personal" />
+      <q-tab name="report" label="Reportes" />
+    </q-tabs>
+    <q-tab-panels v-model="tab">
+      <q-tab-panel name="staff" label="personal">
+        <div class="text-center q-mb-md">
+          <q-btn color="positive" label="Pasar Asistencia" />
+        </div>
+        <StaffComponent />
+      </q-tab-panel>
+      <q-tab-panel name="report" label="Reportes">
+        <ReportComponent />
+      </q-tab-panel>
+    </q-tab-panels>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+import StaffComponent from 'src/components/StaffComponent.vue';
+import ReportComponent from 'src/components/ReportComponent.vue';
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: {
+    StaffComponent,
+    ReportComponent
+  },
+  setup() {
+    return {
+      tab: ref('staff')
+    }
+  }
+})
+</script>
